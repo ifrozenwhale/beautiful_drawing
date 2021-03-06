fdd83c5b782132471f4e2aa75a4cc0b4186649fb
frozenwhale <1614395133@qq.com>
2021-03-07 03:02:29 +0800

    add img

---
+++ README.md
@@ -0,0 +1,6 @@
+# Draw Rose
+- 使用matplotlib绘制3D玫瑰花，借鉴网上代码
+- 创新点在于支持绘制多支玫瑰
+- 绘制心形的玫瑰簇
+
+![avatar](./img/rose521.png)
--- rose-3d.py
+++ rose-3d.py
@@ -6,17 +6,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 dx, dy = 0.215, 0.17
+# dx, dy = 0.2, 0.17
+# dx, dy = 0.087, 0.081
 
 
 def in_love(x, y):
-    return ((x*dx)**2+(y*dy)**2-1)**3 - (x*dx)**2*(y*dy)**3 <= 0
+    # print(((x*0.5)**2+(y*1) ** 2-1)**3-(x*0.5)**2*(y*1)**3 <= 0)
+    return (((x*dx)**2+(y*dy) ** 2-1)**3-(x*dx)**2*(y*dy)**3 <= 0)
+
+
+cnt = 0
 
 
 def draw_rose(fig, ax, i=0, j=0):
     if not in_love(i, j):
         return
-    [x, t] = np.meshgrid(np.array(range(25))/24.0,
-                         np.arange(0, 575.5, 5) / 575 * 17 * np.pi - 2 * np.pi)
+    global cnt
+    cnt += 1
+    [x, t] = np.meshgrid(np.array(range(25)) / 24.0,
+                         np.arange(0, 575.5, 500) / 575 * 17 * np.pi - 2 * np.pi)
     p = (np.pi / 2) * np.exp(-t / (8 * np.pi))
     u = 1 - (1 - np.mod(3.6 * t, 2 * np.pi) / np.pi) ** 4 / 2
     y = 2 * (x ** 2 - x) ** 2 * np.sin(p)
@@ -28,13 +36,23 @@ def draw_rose(fig, ax, i=0, j=0):
 
 
 def scale_img(ax, x_scale=1, y_scale=1, z_scale=1):
+
     scale = np.diag([x_scale, y_scale, z_scale, 1.0])
-    scale *= (1.0/scale.max())
+    scale = scale*(1.0/scale.max())
     scale[3, 3] = 1.0
 
     def short_proj():
         return np.dot(Axes3D.get_proj(ax), scale)
+
     ax.get_proj = short_proj
+
+
+def update_points(num):
+    '''
+    更新数据点
+    '''
+    point_ani.set_data(x[num], y[num])
+    return point_ani,
 
 
 fig = plt.figure(figsize=(15, 10))
@@ -43,7 +61,7 @@ maxv = 8
 plt.axis('off')
 ax.view_init(30, -85)
 plt.tight_layout()
-plt.ion()
+plt.ion()  # interactive mode on
 for i in range(-maxv, maxv):
     for j in range(-maxv, maxv):
         if i == -maxv:
@@ -52,5 +70,9 @@ for i in range(-maxv, maxv):
             scale_img(ax, i+4, maxv+3, 1)
         draw_rose(fig, ax, i, j)
 
+# ax.text(6, 6, 0.5, s="521 Roses from Aoao", fontsize=10)
+# ax.view_init()
 plt.pause(3)
+
+plt.savefig("./img/rose_test.pdf")
 plt.show()
